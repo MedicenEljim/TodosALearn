246e4494484bcbf6a6e14a9f5aab186fd850ffe6
Jimmy Andres Pelaez <141855684+MedicenEljim@users.noreply.github.com>
2024-02-05 15:31:46 -0500

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -49,7 +49,7 @@ class LoginController extends Controller
      */
     public function crearCuenta()
     {
-        return view('Login.crearCuenta');
+        return view('Login.crearcuenta');
     }
 
     /**
